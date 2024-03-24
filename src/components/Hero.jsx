4513096a853b25224d3bd07d3d26a356a66765cd
wtcrawford <wtcmetcode@gmail.com>
2024-03-24 15:00:00 -0700

    Added 'View My Resume' and fixed a typo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
         </div>
         <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>Hi, i'm
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm
             <span className='text-[#915eff]'>Wyatt</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
