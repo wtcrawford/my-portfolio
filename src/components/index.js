78c3ca86c737dbb3c1ec83372b3fd98b00aa45f3
wtcrawford <wtcmetcode@gmail.com>
2024-10-30 16:59:35 -0700

    error fix

--- src/components/index.js
+++ src/components/index.js
@@ -6,7 +6,7 @@ import Tech from './Tech';
 // import Experience from './Experience';
 import Works from './Works';
 import Feedbacks from './Feedbacks';
-import Contact from './Contact';
+// import Contact from './Contact';
 
 export {
   Hero,
